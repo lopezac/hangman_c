e6735ac61f2576eebb8446007ab5524b6485af66
Axel Lopez <lopezaxel@protonmail.com>
2023-04-30 18:07:25 -0300

    Finish game!

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 
 char *dictionary_path = "./english_words.txt";
 char word[MAXWORD];
+char cur_char = ' ';
 int player_lives = 5;
+int letters_left = 0;
 
 // main loop
 int is_game_over(int letters_left);
@@ -21,37 +23,46 @@ int count_lines(FILE *fp);
 int get_random_number(int max_number);
 int get_line_nth(char *s, int line_num, FILE *fp);
 void get_random_word(char *s);
+void start_game(char *s);
 
 int main()
 {
-    int letters_left, user_letter;
+    int user_letter;
     char word_guessed[MAXWORD];
 
-    get_random_word(word);
-    get_empty_word(word_guessed, word);
-    letters_left = strlen(word);
-    printf("%s", word);
-    printf("word_guessed %s\n", word_guessed);
+    start_game(word_guessed);
     while (!is_game_over(letters_left))
     {
         user_letter = get_user_letter();
-        if (strchr(word, user_letter))
-        {
+        if (strchr(word, user_letter) && !strchr(word_guessed, user_letter))
             replace_letter(word, word_guessed, user_letter);
-            letters_left--;
-            printf("word_guessed %s\n", word_guessed);
-        }
         else
         {
             player_lives--;
+            printf("Lives left %d\n", player_lives);
         }
-        // if (is_game_over(letters_left))
-        // {
-        //     printf("Do you want to restart the game?\n");
-        //     restart_game();
-        // }
+        printf("%s\n", word_guessed);
+        if (is_game_over(letters_left))
+        {
+            printf("\nEnter (y) to restart the game\n");
+            user_letter = get_user_letter();
+            if (user_letter == 'y')
+                start_game(word_guessed);
+        }
     }
     return 0;
+}
+
+void start_game(char *word_guessed)
+{
+    get_random_word(word);
+    get_empty_word(word_guessed, word);
+    letters_left = strlen(word) - 2;
+    player_lives = 5;
+    printf("\nWelcome to Hangman Game!\n");
+    printf("Word to guess: %s", word);
+    printf("You have %d lives!\n", player_lives);
+    printf("Enter your guess:\n");
 }
 
 void get_empty_word(char *empty, char *word)
@@ -65,6 +76,7 @@ void get_empty_word(char *empty, char *word)
         *empty++;
         *word++;
     }
+    *empty = '\0';
 }
 
 // replace letter that match letters in s1 in s2
@@ -73,7 +85,10 @@ void replace_letter(char *s1, char *s2, int letter)
     while (*s1 != EOF && *s1 != '\0')
     {
         if (*s1 == letter)
+        {
             *s2 = letter;
+            letters_left--;
+        }
         *s1++;
         *s2++;
     }
@@ -85,6 +100,7 @@ int get_user_letter(void)
 
     while (!isalpha(c = getchar()))
         ;
+    cur_char = tolower(c);
     return tolower(c);
 }
 
